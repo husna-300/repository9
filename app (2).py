d487e34902c3080dfd38f77eecf9016435c952ff
husna <h111126522@gmail.com>
2023-07-17 01:43:37 +0500

    my 9th repo

---
+++ app (2).py
@@ -0,0 +1,13 @@
+if 6 > 2 :
+    print("python")
+    print("a new tool for team.") 
+
+if 10 > 5 :
+    print("correct")
+if 20 == 20 :
+    print("true")
+if 10 < 5 :
+    print("incorrect")
+if 10 == 15 :
+    print("false")
+    print("not equal")
